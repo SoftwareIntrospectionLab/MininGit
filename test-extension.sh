c70568bbc4731cc09a51f7c77c5ddc9c7f685976
Chris Lewis <chris@chris.to>
2010-11-08 19:23:08 -0800

    Added extensions back to test-extension

--- test-extension.sh
+++ test-extension.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 rm test-db.sqlite
-./cvsanaly2 -g --db-driver sqlite --db-database test-db.sqlite ~/Downloads/gitflow/
+./cvsanaly2 -g --db-driver sqlite --db-database test-db.sqlite extensions=$1 ~/Downloads/gitflow/
