c38c23da2cd4a8e60eadae47fd6cede3a7e6bcef
Chris Lewis <chris@chris.to>
2010-11-11 14:51:30 -0800

    Converted help docs to Markdown

--- test-extension.sh
+++ test-extension.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 rm test-db.sqlite
-./cvsanaly2 -g -u root -p root --extensions=Content,Hunks ~/Downloads/voldemort/
+./cvsanaly2 -g -u root -p root --extensions=Content,Hunks git://github.com/voldemort/voldemort.git
