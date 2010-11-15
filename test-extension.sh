0dda4e95f755bfe3865f02fb2338f45259665b78
Chris Lewis <chris@chris.to>
2010-11-14 21:26:14 -0800

    Fixed patches to work for small repos

--- test-extension.sh
+++ test-extension.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 rm test-db.sqlite
-./cvsanaly2 -g -u root -p root --extensions=Content,Hunks ~/Downloads/voldemort/
+./cvsanaly2 -g -u root -p root --extensions=$1 ~/Documents/Computing/Projects/Scala/JMS/.git
