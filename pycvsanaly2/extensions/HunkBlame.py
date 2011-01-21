424b676b7856de2355eb3e0575619e6e659aa5e5
Zhongpeng Lin <zlin5@ucsc.edu>
2011-01-20 18:18:43 -0800

    hunk blame only blame is_bug_fix=1

--- pycvsanaly2/extensions/HunkBlame.py
+++ pycvsanaly2/extensions/HunkBlame.py
@@ -215,6 +215,7 @@ class HunkBlame(Blame):
         outer_query = """select distinct h.file_id, h.commit_id
             from hunks h, scmlog s
             where h.commit_id=s.id and s.repository_id=?
+                and s.is_bug_fix=1
                 and h.old_start_line is not null 
                 and h.old_end_line is not null
                 and h.file_id is not null
