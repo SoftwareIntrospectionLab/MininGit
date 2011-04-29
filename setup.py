e9e91ab7817019057c6a58bfa7dd75166ff3cece
Chris Lewis <chris@chris.to>
2011-04-29 15:59:36 -0700

    Attempt at fixing setup.py and pip

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ setup(
     packages=['pycvsanaly2', 'pycvsanaly2.extensions'],
     long_description=read('README.mdown'),
     scripts = ["cvsanaly2"],
+    install_requires=['repositoryhandler>=0.3.2', 'guilty>=2.0']
+    dependency_links = ['https://github.com/SoftwareIntrospectionLab/repositoryhandler/tarball/develop#egg=repositoryhandler-0.3.2', 'https://github.com/SoftwareIntrospectionLab/guilty#egg=guilty-2.0']
     classifiers = [
         "Development Status :: 4 - Beta",
         "Topic :: Software Development :: Version Control",
