d54f06eee54b98611cb3ffc6521f067a8969ecd0
Chris Lewis <chris@chris.to>
2011-05-04 11:14:55 -0700

    This file was needed, it was used in the help output.
    Revert "This file is not necessary, its probably left over from the automake stuff."
    
    This reverts commit 20168cea30bc9b1ebb2ebc0c259d21d63b7c7f23.

---
+++ pycvsanaly2/_config.py
@@ -0,0 +1,7 @@
+PACKAGE = 'cvsanaly2'
+VERSION = '2.3.0'
+
+AUTHOR = 'LibreSoft'
+AUTHOR_EMAIL = 'libresoft-tools-devel@lists.morfeo-project.org'
+COPYRIGHT = "Copyright (C) 2004-2009 %s <%s>" % (AUTHOR, AUTHOR_EMAIL)
+DESCRIPTION = "An analysis tool for your source code repository"
