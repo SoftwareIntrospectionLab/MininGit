1a7a0cbc15e030a7226e424a857329256cd49672
Alexander Pepper <alexander.pepper@infopark.de>
2011-09-01 10:58:51 +0200

    [HunkBlame] Refactoring of var names.

--- pycvsanaly2/extensions/HunkBlame.py
+++ pycvsanaly2/extensions/HunkBlame.py
@@ -409,13 +409,13 @@ class HunkBlame(Blame):
                 if relative_path is None:
                     # Maybe the file_id did change (especially when working with different branches).
                     # Check if a file with the exact same path exists at pre_commit_id
-                    old_file_name = self.fp.get_path_from_database(file_id, commit_id)
-                    new_file_id = self.fp.get_file_id(old_file_name, commit_id)
-                    if new_file_id is None:
+                    current_path = self.fp.get_path_from_database(file_id, commit_id)
+                    pre_file_id = self.fp.get_file_id(current_path, commit_id)
+                    if pre_file_id is None:
                         raise NotValidHunkWarning("Couldn't find path for " + \
                                                   "file ID %d at %s" % (file_id, pre_rev))
                     else:
-                        relative_path = old_file_name
+                        relative_path = current_path
                 
                 printdbg("Path for %d at %s -> %s", (file_id, pre_rev,
                                                      relative_path))
